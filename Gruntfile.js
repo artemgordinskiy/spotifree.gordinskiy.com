54fcaf6b6681fee6b1db4766f5dca5e1c702d7a7
Artem Gordinsky <artem@gordinskiy.com>
2015-03-08 17:55:52 +0200

    Create a build workflow in Grunt, optimize some stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,100 +1,141 @@
-/*global module:false*/
 module.exports = function(grunt) {
-
-    // Project configuration.
     grunt.initConfig({
-        // Metadata.
         pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' + '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' + '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-        // Task configuration.
-        copy: {
-          main: {
-            files: [
-              // includes files within path and its sub-directories
-              {expand: true, src: ['**'], dest: 'dist/'}
-            ]
-          }
-        },
+
         concat: {
-            options: {
-                banner: '<%= banner %>',
-                stripBanners: true
-            },
-            dist: {
+            js: {
                 src: ['js/*.js'],
                 dest: 'dist/scripts.js'
+            },
+            css: {
+                src: ['css/*.css'],
+                dest: 'dist/styles.css'
             }
         },
+
         uglify: {
+            js: {
+                files: {'dist/scripts.js': ['dist/scripts.js']}
+            }
+        },
+
+        cssmin: {
             options: {
-                banner: '<%= banner %>'
+                keepSpecialComments: false
             },
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist',
+                    src: ['styles.css'],
+                    dest: 'dist',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        htmlmin: {
             dist: {
-                src: 'dist/scripts.js',
-                dest: 'dist/scripts.js'
+                options: {
+                    removeComments: true,
+                    minifyJS: true,
+                    minifyCSS: true,
+                    collapseWhitespace: true
+                }, files: {
+                    'dist/index.html': 'index.html',
+                    'dist/contact.html': 'contact.html',
+                    'dist/about.html': 'about.html',
+                    'dist/troubleshooting.html': 'troubleshooting.html',
+                    'dist/404.html': '404.html'
+                }
             }
         },
-        jshint: {
-            options: {
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                unused: true,
-                boss: true,
-                eqnull: true,
-                browser: true,
-                globals: {}
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            }
-        },
+
         uncss: {
-            options: {
-                ignoreSheets: [/cdnjs.cloudflare.com/]
-            },
             dist: {
+                options: {
+                    ignoreSheets: [/cdnjs\.cloudflare\.com/],
+                    ignore: [/\.active/, /\.open/, /\.disabled/]
+                },
                 files: {
-                    'dist/css/tidy-main.css': ['index.html'],
-                    'dist/css/tidy-secondary.css': ['about.html', 'contact.html', 'troubleshooting.html']
+                    'dist/styles.css': ['index.html', 'about.html', 'contact.html', 'troubleshooting.html']
+                }
+            }
+        },
+
+        'string-replace': {
+            replace_asset_src: {
+                files: {
+                    'dist/': ['dist/*.html']
                 },
                 options: {
-                    compress: true
+                    replacements: [
+                        {
+                            pattern: /rel="stylesheet"\shref="dist\//g,
+                            replacement: 'rel="stylesheet" href="'
+                        },
+                        {
+                            pattern: /src="dist\//g,
+                            replacement: 'src="'
+                        }
+                    ]
                 }
-            },
-            processhtml: {
-              dist: {
-                files: {
-                  'dist/index.html': ['index.html'],
-                  'dist/about.html': ['about.html'],
-                  'dist/contact.html': ['contact.html'],
-                  'dist/troubleshooting.html': ['troubleshooting.html']
-                }
-              }
             }
         },
+
+        compress: {
+            main: {
+                options: {
+                    mode: 'gzip'
+                },
+                expand: true,
+                cwd: 'dist/',
+                src: ['**/*'],
+                dest: 'dist/compressed/'
+            }
+        },
+
+        csscss: {
+            dist: {
+                src: ['dist/styles.css']
+            }
+        },
+
         watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
+            js: {
+                files: ['js/*.js'],
+                tasks: ['concat:js']
+            },
+            css: {
+                files: ['css/*.css'],
+                tasks: ['concat:css']
             }
         }
     });
 
-    // These plugins provide necessary tasks.
-//    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-uncss');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-string-replace');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-csscss');
 
-    // Default task.
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'uncss']);
+    grunt.registerTask('build-dev', [
+        'concat:js',
+        'concat:css'
+    ]);
 
+    grunt.registerTask('build-prod', [
+        'concat:js',
+        'concat:css',
+        'uncss',
+        'uglify',
+        'cssmin',
+        'htmlmin',
+        'string-replace:replace_asset_src',
+        'compress'
+    ]);
 };
